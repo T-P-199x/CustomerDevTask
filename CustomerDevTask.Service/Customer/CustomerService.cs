5f565f40428ec325b78154f1d6297a835b3bdc28
Leonidas199x <toby.w.prince@outlook.com>
2021-03-19 16:45:18 +0000

    Addition of Customer Service

---
+++ CustomerDevTask.Service/Customer/CustomerService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CustomerDevTask.SqlRepository;
+using CustomerDevTask.SqlRepository.Customer;
+
+namespace CustomerDevTask.Service.Customer
+{
+    public class CustomerService : ICustomerService
+    {
+        private ICustomerRepository _customerRepository;
+
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<IEnumerable<Models.Customer.Customer>> GetAll()
+        {
+            return await _customerRepository.GetAll().ConfigureAwait(false);
+        }
+
+        public async Task<Models.Customer.Customer> Get(int Id)
+        {
+            return await _customerRepository.Get(Id).ConfigureAwait(false);
+        }
+
+        public async Task<bool> Update(Models.Customer.Customer customer)
+        {
+            return await _customerRepository.Save(customer, SaveType.Update).ConfigureAwait(false);
+        }
+
+        public async Task<bool> Insert(Models.Customer.Customer customer)
+        {
+            return await _customerRepository.Save(customer, SaveType.Insert).ConfigureAwait(false);
+        }
+    }
+}
---
+++ CustomerDevTask.Service/Customer/ICustomerService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CustomerDevTask.Service.Customer
+{ 
+    public interface ICustomerService
+    {
+        Task<IEnumerable<Models.Customer.Customer>> GetAll();
+
+        Task<Models.Customer.Customer> Get(int Id);
+
+        Task<bool> Update(Models.Customer.Customer customer);
+
+        Task<bool> Insert(Models.Customer.Customer customer);
+    }
+}
